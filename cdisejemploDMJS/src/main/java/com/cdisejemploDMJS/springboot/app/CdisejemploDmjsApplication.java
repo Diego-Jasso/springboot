6fb96ff4a8163ef82aea38859578bddb90a3054e
Diego Marcelo Jasso Sanchez <djasso@ruhrpumpen.com>
2024-04-28 14:31:38 -0600

    Creacion del proyecto y creacion de entidades y tablas en base de datos

---
+++ cdisejemploDMJS/src/main/java/com/cdisejemploDMJS/springboot/app/CdisejemploDmjsApplication.java
@@ -0,0 +1,13 @@
+package com.cdisejemploDMJS.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdisejemploDmjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdisejemploDmjsApplication.class, args);
+	}
+
+}
---
+++ cdisejemploDMJS/src/test/java/com/cdisejemploDMJS/springboot/app/CdisejemploDmjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdisejemploDMJS.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdisejemploDmjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
